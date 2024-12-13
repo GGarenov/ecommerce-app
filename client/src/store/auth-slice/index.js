9b8b783b2c1e795c3298c481f68a6aff35d4264c
Georgi <g_garenov@outlook.com>
2024-12-13 16:18:43 +0200

    The login logic in the redux is created

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -23,6 +23,18 @@ export const registerUser = createAsyncThunk(
   }
 );
 
+export const loginUser = createAsyncThunk("/auth/login", async (formDate) => {
+  const response = await axios.post(
+    "http://localhost:5000/api/auth/login",
+    formDate,
+    {
+      withCredentials: true,
+    }
+  );
+
+  return response.data;
+});
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -40,6 +52,19 @@ const authSlice = createSlice({
         state.isAuthenticated = false;
       })
       .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.isAuthenticated = true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
