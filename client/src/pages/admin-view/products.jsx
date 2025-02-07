8d1f9691057396ec93ae4f05fc9851270ad5d578
Georgi <g_garenov@outlook.com>
2025-02-07 18:48:48 +0200

    addProductFormElements are added in the config file

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -5,9 +5,10 @@ function AdminProducts() {
   return (
     <>
       <Fragment>
-        <div className="mb-5 flex justify-end">
+        <div className="mb-5 w-full flex justify-end">
           <Button>Add new product</Button>
         </div>
+        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
       </Fragment>
     </>
   );
