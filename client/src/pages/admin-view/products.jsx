70bf7531986322fee7b57a908339773d534a1937
Georgi <g_garenov@outlook.com>
2025-02-07 19:04:19 +0200

    side bar is wrapped in Sheet components

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,14 +1,38 @@
 import { Button } from "@/components/ui/button";
-import { Fragment } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+// import { Sheet } from "lucide-react";
+import { Fragment, useState } from "react";
 
 function AdminProducts() {
+  const [openCreateProductsDialog, setOpenCreateProductDialog] =
+    useState(false);
+
   return (
     <>
       <Fragment>
         <div className="mb-5 w-full flex justify-end">
-          <Button>Add new product</Button>
+          <Button onClick={() => setOpenCreateProductDialog(true)}>
+            Add new product
+          </Button>
         </div>
         <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+        <Sheet
+          open={openCreateProductsDialog}
+          onOpenChange={() => {
+            setOpenCreateProductDialog(false);
+          }}
+        >
+          <SheetContent side="right" className="overflow-auto">
+            <SheetHeader>
+              <SheetTitle>Add new product</SheetTitle>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </Fragment>
     </>
   );
