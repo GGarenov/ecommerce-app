8b760807901683aa8fbd305c993fb1b33af79821
Georgi <g_garenov@outlook.com>
2025-02-07 18:44:33 +0200

    product page in admin view is edited

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Fragment } from "react";
+
 function AdminProducts() {
-  return <div>Admin Products</div>;
+  return (
+    <>
+      <Fragment>
+        <div className="mb-5 flex justify-end">
+          <Button>Add new product</Button>
+        </div>
+      </Fragment>
+    </>
+  );
 }
 
 export default AdminProducts;
