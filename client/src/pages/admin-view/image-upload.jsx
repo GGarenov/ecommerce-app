f71331a5092574d2687b09ebf49504afcba29cf3
Georgi <g_garenov@outlook.com>
2025-02-08 13:55:28 +0200

    upload image file is created

---
+++ client/src/pages/admin-view/image-upload.jsx
@@ -0,0 +1,11 @@
+import { Label } from "@radix-ui/react-label";
+
+function ProductImageUpload() {
+  return (
+    <div className="w-full mx-w-md mx-auto">
+      <Label className="text-lg font-semibold mb-2 block">Upload</Label>
+    </div>
+  );
+}
+
+export default ProductImageUpload;
