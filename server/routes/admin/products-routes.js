b86ad13edbb388a3af6a6d5235a258c15fd8aefd
Georgi <g_garenov@outlook.com>
2025-02-19 09:39:58 +0200

    Module export

--- server/routes/admin/products-routes.js
+++ server/routes/admin/products-routes.js
@@ -9,3 +9,5 @@ const { upload } = require("../../helpers/cloudinary");
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+
+module.exports = router;
