7bdf841068ad87dde1b5a0a02a28d7073e39dcf7
Georgi <g_garenov@outlook.com>
2025-02-20 10:59:01 +0200

    Logic added in the store.js

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -64,5 +64,22 @@ const AdminProductsSlice = createSlice({
   name: "adminProducts",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllProducts.fulfilled, (state, action) => {
+        console.log(action.payload);
+
+        state.isLoading = false;
+        state.productList = action.payload;
+      })
+      .addCase(fetchAllProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productList = [];
+      });
+  },
 });
+
+export default AdminProductsSlice.reducer;
