f80a6e191bc405932d1d1b92636ee8ecfcd25b39
Georgi <g_garenov@outlook.com>
2025-02-20 10:44:17 +0200

    Add new product async thunk is created

---
+++ client/src/store/admin/products-slice/index.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  products: [],
+};
+
+export const addNewProduct = createAsyncThunk(
+  "/products/addnewproduct",
+  async (formData) => {
+    const result = await axios.post(
+      "http://localhost:5000/api/products/add",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return result?.data;
+  }
+);
+
+const AdminProductsSlice = createSlice({
+  name: "adminProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
