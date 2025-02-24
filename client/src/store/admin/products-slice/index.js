aac0a331fbe12822cd6d7946c80dc909385eb7f4
Georgi <g_garenov@outlook.com>
2025-02-24 19:05:16 +0200

    All the errors with the wrong links are fixed.

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -26,7 +26,9 @@ export const addNewProduct = createAsyncThunk(
 export const fetchAllProducts = createAsyncThunk(
   "/products/fetchallproducts",
   async () => {
-    const result = await axios.get("http://localhost:5000/api/products/get");
+    const result = await axios.get(
+      "http://localhost:5000/api/admin/products/get"
+    );
 
     return result?.data;
   }
@@ -36,7 +38,7 @@ export const editProduct = createAsyncThunk(
   "/products/editproduct",
   async ({ id, formData }) => {
     const result = await axios.put(
-      `http://localhost:5000/api/products/edit/${id}`,
+      `http://localhost:5000/api/admin/products/edit/${id}`,
       formData,
       {
         headers: {
@@ -53,7 +55,7 @@ export const deleteProduct = createAsyncThunk(
   "/products/deleteProduct",
   async (id) => {
     const result = await axios.delete(
-      `http://localhost:5000/api/products/delete/${id}`
+      `http://localhost:5000/api/admin/products/delete/${id}`
     );
 
     return result?.data;
