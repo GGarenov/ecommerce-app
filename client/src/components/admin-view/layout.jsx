12ee68e105b7a3ffc49c41e66d33a947bded2ae0
Georgi <g_garenov@outlook.com>
2025-04-14 21:17:56 +0300

    Layout is fixed so that the products can be shown better.

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -8,12 +8,12 @@ function AdminLayout() {
 
   return (
     <div className="flex min-h-screen w-full">
-      {/* ädmin sidebar */}
+      {/* admin sidebar */}
       <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
         {/* admin header */}
         <AdminHeader setOpen={setOpenSidebar} />
-        <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
       </div>
