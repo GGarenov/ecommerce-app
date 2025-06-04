d8bdb9154cca01d667cb4ac19895c8f0f543abb2
Georgi <g_garenov@outlook.com>
2025-06-04 18:28:14 +0300

    Start editing the home page and 3 banners are added in the assets

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -1,5 +1,13 @@
+import bannerOne from "../../assets/banner-1.webp";
+import bannerTwo from "../../assets/banner-2.webp";
+import bannerThree from "../../assets/banner-3.webp";
+
 function ShoppingHome() {
-  return <div>Shopping view home</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="relative w-full h-[600px] overflow-hidden">{}</div>
+    </div>
+  );
 }
 
 export default ShoppingHome;
