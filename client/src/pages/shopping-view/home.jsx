e6d28d5e0d7ed6af904238829fe9b7a8cc00272b
Georgi <g_garenov@outlook.com>
2025-06-04 19:01:57 +0300

    Home page still in progress

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -1,11 +1,93 @@
+import { Button } from "@/components/ui/button";
 import bannerOne from "../../assets/banner-1.webp";
 import bannerTwo from "../../assets/banner-2.webp";
 import bannerThree from "../../assets/banner-3.webp";
+import {
+  BabyIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  CloudLightning,
+  FootprintsIcon,
+  ShirtIcon,
+  UmbrellaIcon,
+  WatchIcon,
+} from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import { useEffect, useState } from "react";
 
 function ShoppingHome() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slides = [bannerOne, bannerTwo, bannerThree];
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 2000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const categoriesWithIcon = [
+    { id: "men", label: "Men", icon: ShirtIcon },
+    { id: "women", label: "Women", icon: CloudLightning },
+    { id: "kids", label: "Kids", icon: BabyIcon },
+    { id: "accessories", label: "Accessories", icon: WatchIcon },
+    { id: "footwear", label: "Footwear", icon: FootprintsIcon },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="relative w-full h-[600px] overflow-hidden">{}</div>
+      <div className="relative w-full h-[600px] overflow-hidden">
+        {slides.map((slide, index) => (
+          <img
+            src={slide}
+            key={index}
+            className={`${
+              index === currentSlide ? "opacity-100" : "opacity-0"
+            }absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
+          />
+        ))}
+        <Button
+          valiant="outline"
+          size="icon"
+          onClick={() =>
+            setCurrentSlide(
+              (prevSlide) => (prevSlide - 1 + slides.length) % slides.length
+            )
+          }
+          className="absolute top-1/2 left-4 transform -translate-y-1/2"
+        >
+          <ChevronLeftIcon className="w-4 h-4" />
+        </Button>
+        <Button
+          valiant="outline"
+          size="icon"
+          onClick={() =>
+            setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length)
+          }
+          className="absolute top-1/2 right-4 transform -translate-y-1/2"
+        >
+          <ChevronRightIcon className="w-4 h-4" />
+        </Button>
+      </div>
+      <section className="py-12 bg-gray-50">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-8">
+            Shop by category
+          </h2>
+          <div className="grid grid-cols-2 md:grid-col-3 lg:grid-cols-5 gap-4">
+            {categoriesWithIcon.map((categoryItem) => (
+              <Card className="cursor-pointer hover:shadow-lg transition-shadow">
+                <CardContent className="flex flex-col items-center justify-center p-6">
+                  <categoryItem.icon className="w-12 h-12 mb-4 text-primary" />
+                  <span className="font-bold">{categoryItem.label}</span>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
