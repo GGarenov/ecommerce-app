e15e5575678998a66c21aaeadbc43201dcfb7841
Georgi <g_garenov@outlook.com>
2025-06-04 13:47:54 +0300

    Cart items content page is designed successfully

--- client/src/components/shopping-view/cart-items-content.jsx
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -1,5 +1,48 @@
-function UserCartItemsContent() {
-  return <div>UserCartItemsContent</div>;
+import { Minus, Plus, Trash } from "lucide-react";
+import { Button } from "../ui/button";
+
+function UserCartItemsContent({ cartItem }) {
+  return (
+    <div className="flex items-center space-x-4">
+      <img
+        src={cartItem?.image}
+        alt={cartItem?.title}
+        className="w-20 h-20 rounded object-cover"
+      />
+      <div className="flex-1">
+        <h3 className="font-extrabol">{cartItem?.title}</h3>
+        <div className="flex items-center mt-1">
+          <Button
+            className="h-8 w-8 rounded-full"
+            variant="outline"
+            size="icon"
+          >
+            <Minus className="w-4 h-4" />
+            <span className="sr-only">Decrease</span>
+          </Button>
+          <span className="font-semibold mr-2 ml-2">{cartItem?.quantity}</span>
+          <Button
+            className="h-8 w-8 rounded-full"
+            variant="outline"
+            size="icon"
+          >
+            <Plus className="w-4 h-4" />
+            <span className="sr-only">Increase</span>
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col items-end">
+        <p className="font-semibold">
+          $
+          {(
+            (cartItem?.salePrice > 0 ? cartItem?.salePrice : cartItem?.price) *
+            cartItem?.quantity
+          ).toFixed(2)}
+        </p>
+        <Trash className="cursor-pointer mt-1" size={20} />
+      </div>
+    </div>
+  );
 }
 
 export default UserCartItemsContent;
