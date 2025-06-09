0f86d7d3a8d8b1989ccbc67fdfda34431768f4cd
Georgi <g_garenov@outlook.com>
2025-06-09 11:57:28 +0300

    Address model is created.

---
+++ server/models/Address.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const AddressSchema = new mongoose.Schema(
+  {
+    userId: String,
+    address: String,
+    city: String,
+    pincode: String,
+    phone: Number,
+    notes: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Address", AddressSchema);
