e570bdbf57f23eda344f003c95e61723827d05ec
Georgi <g_garenov@outlook.com>
2025-06-09 12:19:58 +0300

    The address controllers are created and now have to be implemented in the async thunk in the redux.

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -1,56 +1,135 @@
+const Address = require("../../models/Address");
 
+const addAddress = async (req, res) => {
+  try {
+    const { userId, address, city, pincode, phone, notes } = req.body;
 
-const addAddress = async(req, res) = {
-
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'Error'
-        })  
+    if (!userId || !address || !city || !pincode || !phone || !notes) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
     }
-}
 
-const fetchAllAddress = async(req, res) = {
+    const newlyCreatedAddress = new Address({
+      userId,
+      address,
+      city,
+      pincode,
+      notes,
+      phone,
+    });
 
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'Error'
-        })  
+    await newlyCreatedAddress.save();
+
+    res.status(201).json({
+      success: true,
+      data: newlyCreatedAddress,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+const fetchAllAddress = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "User id is required!",
+      });
     }
-}
 
+    const addressList = await Address.find({ userId });
 
-const editAddress = async(req, res) = {
+    res.status(200).json({
+      success: true,
+      data: addressList,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'Error'
-        })  
+const editAddress = async (req, res) => {
+  try {
+    const { userId, addressId } = req.params;
+    const formData = req.body;
+    if (!userId || !addressId) {
+      return res.status(400).json({
+        success: false,
+        message: "User and address id is required!",
+      });
     }
-}
 
-const deleteAddress = async(req, res) = {
+    const address = await Address.findOneAndUpdate(
+      {
+        _id: addressId,
+        userId,
+      },
+      formData,
+      { new: true }
+    );
 
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            success: false,
-            message: 'Error'
-        })  
+    if (!address) {
+      return res.status(401).json({
+        success: false,
+        message: "Address is not found!",
+      });
     }
-}
 
-module.exports = {addAddress, fetchAllAddress, editAddress, deleteAddress}
+    res.status(200).json({
+      success: true,
+      data: address,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+const deleteAddress = async (req, res) => {
+  try {
+    const { userId, addressId } = req.params;
+    if (!userId || !addressId) {
+      return res.status(400).json({
+        success: false,
+        message: "User and address id is required!",
+      });
+    }
+
+    const address = await Address.findByIdAndDelete({ _id: addressId, userId });
+
+    if (!address) {
+      return res.status(401).json({
+        success: false,
+        message: "Address is not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Address deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+module.exports = { addAddress, fetchAllAddress, editAddress, deleteAddress };
