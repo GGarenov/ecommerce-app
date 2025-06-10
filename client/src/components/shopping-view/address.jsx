c545ca0eab6ee15892d0d9137b774de634b2a238
Georgi <g_garenov@outlook.com>
2025-06-10 09:43:36 +0300

    Address form control is added in the config file.

--- client/src/components/shopping-view/address.jsx
+++ client/src/components/shopping-view/address.jsx
@@ -1,5 +1,40 @@
+import { addressFormControls } from "@/config";
+import CommonForm from "../common/form";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { useState } from "react";
+
+const initialAddressFormData = {
+  address: "",
+  city: "",
+  phone: "",
+  pincode: "",
+  notes: "",
+};
+
 function Address() {
-  return <div>Address</div>;
+  const [formData, setFormData] = useState(initialAddressFormData);
+
+  function handleManageAddress(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <Card>
+      <div>Address List</div>
+      <CardHeader>
+        <CardTitle>Add new address</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-3">
+        <CommonForm
+          formControls={addressFormControls}
+          formData={formData}
+          setFormData={setFormData}
+          buttonText={"Add"}
+          onSubmit={handleManageAddress}
+        />
+      </CardContent>
+    </Card>
+  );
 }
 
 export default Address;
