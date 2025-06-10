a1ef7aed6cae1cf9233b6f8679e64578fd6b26b6
Georgi <g_garenov@outlook.com>
2025-06-10 09:30:42 +0300

    Start working on the account page and a image for the account page has been added.

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -1,5 +1,29 @@
+import accountImg from "../../assets/account.jpg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function ShoppingAccount() {
-  return <div>Shopping account page</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img
+          src={accountImg}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
+        <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+          <Tabs defaultValue="orders">
+            <TabsList>
+              <TabsTrigger value="orders">Orders</TabsTrigger>
+              <TabsTrigger value="address">Address</TabsTrigger>
+            </TabsList>
+            <TabsContent value="orders">Nekvi poruchki</TabsContent>
+            <TabsContent value="address">Nekvi adresi</TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ShoppingAccount;
