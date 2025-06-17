0967a65555061449ed9d85b442012fbdd93ef3bc
Georgi <g_garenov@outlook.com>
2025-06-17 11:59:25 +0300

    WOrking on the paypal-return page.

--- client/src/pages/shopping-view/paypal-return.jsx
+++ client/src/pages/shopping-view/paypal-return.jsx
@@ -1,5 +1,21 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+
 function PaypalReturnPage() {
-  return <div>Paypal Return Page</div>;
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const paymentId = params.get("paymentId");
+  const payerId = params.get("PayerId");
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Processing Payment... Please wait</CardTitle>
+      </CardHeader>
+    </Card>
+  );
 }
 
 export default PaypalReturnPage;
