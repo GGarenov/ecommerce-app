49713fba1442cb0bbdd83aa5b16289fe8c46af9e
Georgi <g_garenov@outlook.com>
2025-06-17 22:53:48 +0300

    Two more functions are added in the backend for admin orders

--- server/controllers/admin/order-controller.js
+++ server/controllers/admin/order-controller.js
@@ -24,4 +24,30 @@ const getAllOrders = async (req, res) => {
   }
 };
 
-module.exports = { getAllOrders };
+const getOrderDetailsForAdmin = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
+
+module.exports = { getAllOrders, getOrderDetailsForAdmin };
--- server/routes/admin/order-routes.js
+++ server/routes/admin/order-routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
-const { getAllOrders } = require("../../controllers/admin/order-controller");
+const {
+  getAllOrders,
+  getOrderDetailsForAdmin,
+} = require("../../controllers/admin/order-controller");
 
 const router = express.Router();
 
 router.get("/get", getAllOrders);
+router.get("/details/:id", getOrderDetailsForAdmin);
 
 module.exports = router;
